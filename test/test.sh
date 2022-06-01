1c59afebd13089c7d8de62dd69d7b68e7ffcc20e
songzewei <songzewei@genomics.cn>
2022-06-01 15:13:10 +0800

    change to a nicely testing script

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
+clear
 
+echo ===== STEP 1 =====
+echo Generate random sequences and three contigs
 rm -r tst/
 rm -r contigs/
 python zetaseq/test/spawn_test.py
@@ -9,20 +12,37 @@ mv tst_ctg_1.fa.gz contigs/
 mv tst_ctg_2.fa.gz contigs/
 ls -hl contigs/
 
+read -n 1 -s -r -p "Press any key to continue"
+echo ""
+echo ===== STEP 2 =====
 echo Create a working folder for DereCo
 zetaseq/dereco create -i contigs/ -o tst/ -s
 ls -hl tst/
 
-echo Calculate the NR set
+read -n 1 -s -r -p "Press any key to continue"
+echo ""
+echo ===== STEP 3 =====
+echo Calculate the none-redundant set
 zetaseq/dereco devour -i tst/ -t 4
 ls -hl tst/
 ls -hl tst/dereco_final.fa
-more tst/options.json
 
+read -n 1 -s -r -p "Press any key to continue"
+echo ""
+echo ===== STEP 4 =====
 echo Add a new contig and update the working folder
 mv tst_ctg_3.fa.gz contigs/
 zetaseq/dereco update -i tst/
 
-echo Calculate again
+read -n 1 -s -r -p "Press any key to continue"
+echo ""
+echo ===== STEP 5 =====
+echo Calculate the NR set again
 zetaseq/dereco devour -i tst/ -t 4
 ls -hl tst/
+
+read -n 1 -s -r -p "Press any key to continue"
+echo ""
+echo ===== STEP 6 =====
+echo Check the status of a working folder
+zetaseq/dereco status -i tst/
